44a6544fcca04cc085a5c09e3d2ea83f5acb1d37
deepak05kr18 <122279122+deepak05kr18@users.noreply.github.com>
2023-05-25 22:56:15 +0530

    second commit

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -22,8 +22,6 @@ const userSchema=new mongoose.Schema({
     password: String
 });
 
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -43,7 +41,7 @@ app.get("/register", function(req , res){
 app.post("/register", function(req, res){
     const newUser =new User({
         email: req.body.username,
-        password:req.body.password
+        password: md5(req.body.password)
     });    
     newUser.save()
     .then(function(){
@@ -57,7 +55,7 @@ app.post("/register", function(req, res){
 
 app.post("/login" , function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username})
     .then(function(foundUser){
